b4c227dec1e08c06db72b3aa3b14eaaf9348fe0c
parpit29 <p.arpit29@gmail.com>
2016-07-15 10:22:25 +0530

    first commit

---
+++ app/src/main/java/com/viewpoint/miband/MiActivity.java
@@ -0,0 +1,105 @@
+package com.viewpoint.miband;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothAdapter.LeScanCallback;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+
+public class MiActivity extends Activity implements LeScanCallback {
+
+	private BluetoothAdapter mBluetoothAdapter;
+	private boolean mScanning;
+	private TextView mTextView;
+	private Handler mHandler = new Handler();
+	// Stops scanning after 10 seconds.
+	private static final long SCAN_PERIOD = 10000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActionBar().hide();
+		setContentView(R.layout.activity_mi);
+		mTextView = (TextView) findViewById(R.id.text_search);
+		mBluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE))
+				.getAdapter();
+
+
+
+		//if(Build.VERSION.SDK_INT>=23)
+        //RequestPermission();
+
+	}
+//FOR SDK >=23
+	/*
+	@TargetApi(23)
+	public void RequestPermission() {
+		this.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},1);
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		if (requestCode == 1) {
+			if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+				Log.d("MYLOGS","permission granted");
+
+			}
+		}
+	}*/
+
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		scanLeDevice(true);
+	}
+
+	@SuppressWarnings("deprecation")
+	// L Apis are buggy and crap!
+	private void scanLeDevice(final boolean enable) {
+		if (enable) {
+			mTextView.setText(R.string.looking_for_miband);
+			// Stops scanning after a pre-defined scan period.
+			mHandler.postDelayed(new Runnable() {
+
+				@Override
+				public void run() {
+					mScanning = false;
+					mBluetoothAdapter.stopLeScan(MiActivity.this);
+					mTextView.setText(R.string.not_found);
+				}
+			}, SCAN_PERIOD);
+
+			mScanning = true;
+			mBluetoothAdapter.startLeScan(this);
+		} else {
+			mScanning = false;
+			mBluetoothAdapter.stopLeScan(this);
+		}
+	}
+
+	@Override
+	public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+		if (device != null && device.getName() != null
+				& device.getName().contains("MI")) {
+			System.out.println(device.getAddress());
+			scanLeDevice(false);
+			Intent intent = new Intent(getApplicationContext(), MiOverviewActivity.class);
+			intent.putExtra("address", device.getAddress());
+			startActivity(intent);
+			finish();
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		scanLeDevice(false);
+	}
+}
